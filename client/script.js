dd10c54f813fb6be822037fc82f3f974d7cb3fd8
raptor-2001 <pratham007yadav@gmail.com>
2022-12-26 15:02:47 +0530

    server deployed

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);   
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://code-master.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
